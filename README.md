4ad3ad9c338a0fe456b323bf39128a093e9f60c2
ValePV <va.perez19@hotmail.com>
2018-01-25 20:36:10 -0300

    Arreglos para corrección

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
-# Cifrado César
-### Para cifrar y decifrar un mensaje, seguir los siguientes pasos:
+# GENESIS
+### Aquí comienza tu búsqueda para ver películas
 
-+ Preguntamos al usuario si quiere cifrar o decifrar su frase, codigo, palabra, etc..
-  - Solicitar al usuario que ingrese la opción que desea 
-  - El ingreso debe estar dentro de la opción 1 o 2, si no aparecera un mensaje de "Ingrese una opción valida"
-  - El mensaje aparecera hasta que el usuario ingrese una opción valida
-+ Si el usuario selecciona opción cifrado, la frase ingresada será cifrada.
-+ Si el usuario selecciona opción decifrado, la frase ingresada será decifrada
++ Página para que los usuarios puedan hacer una investigación antes de decidir que película ver.
+
++ Esta página nació, ya que hay personas que hacen un previo estudio para la toma de decisiones al momento de ver una película.
+Por ejemplo: antes de ir a ver una película o abrir las páginas con servicios de peliculas online, se revisa los comentarios y calificaciones de otros usuarios, con ello tenemos una noción de si la película es buena o mala.  
+  - Validamos esta idea, recurriendo a otros squad y consultando ¿hacen una previa investigación de la película antes de verla? ¿En que información se basan al tomar la decisión? ¿Es relevante para ustedes conocer la opinión de otras personas con respecto a la película? ¿Qué datos encuentran relevantes les gustaría que se mencionara acerca de la película?
+
++ Organización
+  - Día Martes 
+    + Planificamos el trabajo
+      - Definimos en que nos íbamos a enfocar
+      - Definir paleta de colores
+      - Investigación de la API y Firebase
+    + División de trabajo
+      - Lía Cubillos - Encargada de UX Design - y parte de maquetación
+      - Aileen Guerra - Front end developer - trabajo con API
+      - Lucía Lagos - Front end developer - trabajo con Firebase (sign in)
+      - Valeska Pérez - Front end developer - trabajo con API
+  - Día Miércoles
+    + Aplicar lo que se investigo el día martes
+    + vista de filtros, log in, sign in
+
++ Hasta hoy llevamos
+  - Parte de Maquetación
+  - Log in con firebase
+  - Mostrar trailers
+
++ Qué esperamos para el día viernes
+  - Tener maquetación completa
+  - Funcionalidad
+  - Que el usuario pueda leer datos de las peliculas y ver su trailer
+  - Que el usuario se pueda logear
+  - Que el usuario ya registrado pueda comentar
+
+
--- cifradoCesar.js
+++ cifradoCesar.js
@@ -1,51 +1,51 @@
-function request(option){
+function request() {
   // solicitar que indique que es lo que desea realizar, cifrar o decifrar
-  do{
-    var responce = prompt("Indique el número de lo que desea hacer: 1)cifrar - 2)decifrar");
-    if(responce != ""){
-   // si se responde cifrado, pasa a la función cipher
-      if(responce == "1") {
+  do {
+    var responce = prompt('Indique el número de lo que desea hacer: 1)cifrar - 2)decifrar');
+    if (responce !== '') {
+      // si se responde cifrado, pasa a la función cipher
+      if (responce === '1') {
         cipher();
-     // si se responde decifrar, pasa la función decipher
-      } else if (responce == "2") {
+      // si se responde decifrar, pasa la función decipher
+      } else if (responce === '2') {
         decipher();
       // si se coloca una opción diferente, se alerta el mensaje
       } else {
-        alert("Ingrese una opción válida");
-      }
+        alert('Ingrese una opción valida');
+      } 
     }
-    // si no se responde nada, se vuelve a preguntar
-  } while (responce == "" || (responce != "1" && responce != "2"));
+  // si no se responde nada, se vuelve a preguntar
+  } while (responce === '' || (responce !== '1' && responce !== '2'));
 }
 
-function cipher(a){
- // variable para preguntar cual es la palabra a cifrar
- var resCiper = prompt("Indique la palabra que desea cifrar")
- // variable para resultado
- var myCipher = String.fromCharCode(temp);
+function cipher() {
+  // variable para preguntar cual es la palabra a cifrar
+  var resCiper = (prompt('Indique la palabra que desea cifrar')).toUpperCase();
+  // crear un array para recorrer
+  var arrCiper = resCiper.split('');
+  var myCipher = [];
   // recorrer la palabra
-  for( i=0; i<resCiper.length; i++ ) {
-    // variable para separar letras  
-     var separ = resCiper.split("")
-     // variables para localizar letras separarlas
-     var temp = separ[i].charCodeAt;
-    // condicional si la letra esta en un lugar superior a 65 - 65 + 33)% + 65
-      if( temp >= 65 )
-     temp = ((temp[i] - 65 + 33)%26)+65;
-     myCipher+=(temp)
-     
-     }
-   alert(myCipher);
+  for (i = 0; i < arrCiper.length; i++) {
+    myCipher.push(String.fromCharCode((((arrCiper[i].charCodeAt() - 65) + 33) % 26) + 65));
+    var finalPhrase1 = myCipher.join('');
+  }
+
+   
+  alert('Tu palabra cifrada es ' + finalPhrase1);
 }
 
-function decipher(){
-  //varibale para preguntar cual es la palabra a decifrar
-  var resDecipher = prompt("Indique la palabra que desea decifrar")
-  var myDecipher = "";
+function decipher() {
+  // varibale para preguntar cual es la palabra a decifrar
+  var resDecipher = (prompt('Indique la palabra que desea decifrar')).toUpperCase();
+  var arrDecipher = resDecipher.split('');
+  var myDecipher = [];
   
-
-  alert(myDecipher);
+  for (i = 0; i < arrDecipher.length; i++) {
+    myDecipher.push(String.fromCharCode(((((arrDecipher[i].charCodeAt() - 65) - 7) + 52) % 26) + 65));
+    var finalPhrase2 = myDecipher.join('');
+  }
   
+  alert(('Tu palabra descifrada es ' + finalPhrase2).toLowerCase());
 }
 
-console.log(request())
+request();
